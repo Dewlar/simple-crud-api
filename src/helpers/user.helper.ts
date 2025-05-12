dc3f290772027a776254c6019a36fa6365844850
Andrei Markouski <insawm@gmail.com>
2025-05-13 01:00:05 +0300

    feat: add get user

--- src/helpers/user.helper.ts
+++ src/helpers/user.helper.ts
@@ -1,4 +1,21 @@
+import { ServerResponse } from 'http';
+import { validate } from 'uuid';
 import { User } from '../models/user.model.js';
+
+export let mockUsers: User[] = [
+  {
+    id: '1',
+    username: 'test1',
+    age: 22,
+    hobbies: ['sumo', 'run', 'sleep'],
+  },
+  {
+    id: '2',
+    username: 'test2',
+    age: 33,
+    hobbies: ['eat', 'listen music'],
+  },
+];
 
 export function isUserType(obj: any): obj is User {
   return typeof obj === 'object' &&
@@ -8,4 +25,26 @@ export function isUserType(obj: any): obj is User {
     typeof obj.age === 'number' &&
     Array.isArray(obj.hobbies) &&
     obj.hobbies.every((hobby: any) => typeof hobby === 'string');
+}
+
+export function isUserValid(res: ServerResponse, userId: string): boolean {
+  const userIds = mockUsers.map((user) => user.id);
+
+  if (!validate(userId)) {
+    respondWithError(res, 400, 'UserId is not valid');
+    return false;
+  }
+
+  const userExists = userIds.some(id => id === userId)
+  if (!userExists) {
+    respondWithError(res, 404, "User with such id doesn't exist");
+    return false;
+  }
+
+  return true;
+}
+
+function respondWithError(res: ServerResponse, statusCode: number, message: string) {
+  res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({ message }));
 }
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,13 @@
 import http, { IncomingMessage, ServerResponse } from 'http';
 import { config } from 'dotenv';
 import { getParsedBody } from './helpers/requests.helper.js';
-import { isUserType } from './helpers/user.helper.js';
+import { isUserType, isUserValid, mockUsers } from './helpers/user.helper.js';
 import { User } from './models/user.model.js';
 import { v4 as uniqId } from 'uuid';
 
 config();
 
 const PORT = process.env.PORT || 30332;
-let mockUsers: User[] = [
-  {
-    id: '1',
-    username: 'test1',
-    age: 22,
-    hobbies: ['sumo', 'run', 'sleep'],
-  },
-  {
-    id: '2',
-    username: 'test2',
-    age: 33,
-    hobbies: ['eat', 'listen music'],
-  },
-];
 
 const server = http.createServer(
   async (req: IncomingMessage, res: ServerResponse) => {
@@ -49,6 +35,21 @@ const server = http.createServer(
             })
           );
         }
+      } else if (req.method === 'GET' && req.url?.startsWith('/users/')) {
+        const path = req.url;
+        const userId = path.replace('/users/', '');
+        if (isUserValid(res, userId)) {
+          const user = mockUsers.find((currUser) => currUser.id === userId);
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify(user));
+        }
+      } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(
+          JSON.stringify({
+            message: `Such resource not found. Please check the URL`,
+          })
+        );
       }
     } catch (err) {
       res.writeHead(500, { 'Content-Type': 'application/json' });
